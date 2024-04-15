8282d2e3d3d54616656e66d7572b0f7b5356cbe8
marinodilauro <m.dilauro90@gmail.com>
2024-04-15 16:00:11 +0200

    Add radio button to filter hotels

--- index.php
+++ index.php
@@ -40,6 +40,42 @@
 
 ];
 
+$parkingFilter = $_GET['parking'];
+var_dump($parkingFilter);
+
+
+$filteredHotels = [];
+
+switch ($parkingFilter) {
+
+  case 'option2':
+    $hasParking = true;
+    var_dump($hasParking);
+    foreach ($hotels as $hotel) {
+      if ($hotel['parking'] === $hasParking) {
+        array_push($filteredHotels, $hotel);
+      }
+    }
+    $hotels = $filteredHotels;
+    break;
+
+  case 'option3':
+    $hasParking = false;
+    var_dump($hasParking);
+    foreach ($hotels as $hotel) {
+      if ($hotel['parking'] === $hasParking) {
+        array_push($filteredHotels, $hotel);
+      }
+    }
+    $hotels = $filteredHotels;
+    break;
+  
+  default:
+     $hotels = $hotels;
+    break;
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -55,47 +91,92 @@
 </head>
 
 <body>
+
+<!-- Filter hotels -->
+<div class="container mb-5">
+
+  <h4>Filter Hotels</h4>
+
+  <form action="" method="GET">
   
-<div class="container mt-5">
-  <div class="row row-cols-5">
-
-  <?php foreach ($hotels as $hotel) : ?>
-
-    <div class="col">
-      <div class="card">
-
-        <div class="card-header">
-          <h4 class="card-title"> <?php echo $hotel['name'] ?> </h4>
-        </div>
-
-        <div class="card-body">
-
-          <p class="card-text"> <?php echo $hotel['description'] ?> </p>
-          
-        </div>
-        
-        <div class="card-footer text-muted">
-          
-          <?php if ($hotel['parking']) : ?>
-            <p class="card-text"> Parcheggio custodito: SI </p>
-            
-            <?php else : ?>
-              <p class="card-text"> Parcheggio custodito: NO </p>
-
-          <?php endif; ?>
-
-          <p> Vote: <?php echo $hotel['vote'] ?>/5 </p>
-          <p> Distance to center: <?php echo $hotel['distance_to_center'] ?> Km </p>
-
-        </div>
-
-      </div>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="radio" name="parking" id="allHotels" value="option1" checked/>
+      <label class="form-check-label" for="allHotels">All</label>
     </div>
 
-  <?php endforeach; ?>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="radio" name="parking" id="parkingYes" value="option2"/>
+      <label class="form-check-label" for="parkingYes">Has parking</label>
+    </div>
 
-  </div>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="radio" name="parking" id="parkingNo" value="option3"/>
+      <label class="form-check-label" for="parkingNo">Doesn't have parking</label>
+    </div>
+    
+    <button type="submit">Filter</button>
+
+  </form>
+
 </div>
+
+
+<!-- Hotel table -->
+
+
+  <div class="container">
+
+    <div class="table-responsive">
+
+        <table class="table table-primary">
+          
+          <thead>
+            <tr>
+
+              <th scope="col">Name</th>
+              <th scope="col">Description</th>
+              <th scope="col">Parking</th>
+              <th scope="col">Vote</th>
+              <th scope="col">Distance to center</th>
+
+            </tr>
+          </thead>
+
+          <tbody>
+
+            <?php foreach ($hotels as $hotel) : ?>
+
+              <tr>
+
+                <td scope="row"> <?php echo $hotel['name'] ?> </td>
+                <td> <?php echo $hotel['description'] ?> </td>
+                <td> <?php echo $hotel["parking"] ? 'Si' : 'No'; ?> </td>
+                <td> <?php echo $hotel['vote'] ?>/5 </td>
+                <td> <?php echo $hotel['distance_to_center'] ?> Km </td>
+
+                
+              </tr>
+
+            <?php endforeach; ?>
+
+          </tbody>
+
+        </table>
+        
+      </div>
+
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+  <!-- Bootstrap JS link -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 
